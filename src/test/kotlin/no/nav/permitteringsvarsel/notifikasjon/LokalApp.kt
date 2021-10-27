7082acca2046e44cf8943027cf28c8ec1861f382
André Mossige <andre.mossige@nav.no>
2021-10-27 12:00:29 +0200

    Legg til skallfiler for kjøring i miljø, lokalt og tester

---
+++ src/test/kotlin/no/nav/permitteringsvarsel/notifikasjon/LokalApp.kt
@@ -0,0 +1,11 @@
+package no.nav.permitteringsvarsel.notifikasjon
+
+// Brukes for å kjøre appen lokalt
+fun main() {
+    startLokalApp()
+}
+
+// Brukes for å kjøre appen i tester
+fun startLokalApp() {
+    App().start()
+}
