eec0613d466effae7894c163e2bd424f1fe4e8da
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-04-26 13:57:29 +0200

    Endret tittel og merkelapper

--- src/main/kotlin/no/nav/permitteringsmelding/notifikasjon/kafka/MeldingType.kt
+++ src/main/kotlin/no/nav/permitteringsmelding/notifikasjon/kafka/MeldingType.kt
@@ -1,7 +1,7 @@
 package no.nav.permitteringsmelding.notifikasjon.kafka
 
 enum class MeldingType(val merkelapp: String, val tittel: String) {
-    MASSEOPPSIGELSE("Masseoppsigelse", "Masseoppsigelse"),
-    PERMITTERING_UTEN_LØNN("Permittering", "Permittering"),
-    INNSKRENKNING_I_ARBEIDSTID("Innskrenkning i arbeidstid", "Innskrenkning i arbeidstid")
+    MASSEOPPSIGELSE("Nedbemanning", "Melding om oppsigelse"),
+    PERMITTERING_UTEN_LØNN("Permittering", "Melding om permittering"),
+    INNSKRENKNING_I_ARBEIDSTID("Innskrenking av arbeidstid", "Melding om innskrenking av arbeidstid")
 }
